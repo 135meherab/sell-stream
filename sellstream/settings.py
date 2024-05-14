5bb197da8ad26e54f046e1475120aacee8a87fb7
meherab <hunterkali66@gmail.com>
2024-05-14 12:19:55 +0600

    updated

--- sellstream/settings.py
+++ sellstream/settings.py
@@ -117,6 +117,7 @@ DATABASES = {
         "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
+DATABASES['default'] = dj_database_url.config()
 
 # DATABASES = {
 #     'default': dj_database_url.config(os.getenv('DATABASE_URL')),
@@ -175,7 +176,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
