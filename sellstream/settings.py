731f0824a2cc56fcea8a97cc4024d68031dce865
meherab <hunterkali66@gmail.com>
2024-05-14 10:52:34 +0600

    updated

--- sellstream/settings.py
+++ sellstream/settings.py
@@ -119,7 +119,7 @@ WSGI_APPLICATION = 'sellstream.wsgi.app'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(os.getenv('DATABASE_URL')),
 }
 
 # DATABASES = {
@@ -174,7 +174,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+# STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
